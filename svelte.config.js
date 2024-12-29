2e82f993fb5db634aba839f77bfc6660efb9b12a
Flakes254 <muriuki.denis21@students.dkut.ac.ke>
2024-12-29 12:26:45 +0300

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      handleMissingId: 'warn'
+    }
   },
   preprocess: vitePreprocess()
 };
+
 export default config;
