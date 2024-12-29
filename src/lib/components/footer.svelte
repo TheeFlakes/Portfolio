71824a6001fa808ab19fe130d78a106fce33f002
Flakes254 <muriuki.denis21@students.dkut.ac.ke>
2024-12-29 09:57:27 +0300

    updates

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -8,22 +8,22 @@
     
     <!-- Footer Right Section -->
     <div class="flex space-x-4">
-      <a href="https://www.linkedin.com/in/denis-mwangi-a3aa9a1a9/" target="_blank" class="hover:text-purple-500">
+      <a href="https://www.linkedin.com/in/denis-mwangi-a3aa9a1a9/" target="_blank" class="hover:text-purple-500" aria-label="LinkedIn Profile">
         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm13.5 11.28h-3v-5.6c0-1.34-.03-3.07-1.87-3.07-1.87 0-2.16 1.46-2.16 2.97v5.7h-3v-10h2.88v1.37h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.6v5.57z"/>
         </svg>
       </a>
-      <a href="https://github.com/TheeFlakes254" target="_blank" class="hover:text-purple-500">
+      <a href="https://github.com/TheeFlakes254" target="_blank" class="hover:text-purple-500" aria-label="GitHub Profile 1">
         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 0c-6.63 0-12 5.37-12 12 0 5.3 3.44 9.8 8.21 11.39.6.11.82-.26.82-.58 0-.29-.01-1.05-.02-2.06-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.34-1.76-1.34-1.76-1.09-.75.08-.74.08-.74 1.2.08 1.83 1.23 1.83 1.23 1.07 1.83 2.81 1.3 3.5.99.11-.78.42-1.3.76-1.6-2.67-.3-5.47-1.34-5.47-5.95 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.17 0 0 1.01-.32 3.3 1.23.96-.27 1.98-.4 3-.4 1.02 0 2.04.14 3 .4 2.29-1.55 3.3-1.23 3.3-1.23.66 1.65.24 2.87.12 3.17.77.84 1.24 1.91 1.24 3.22 0 4.62-2.81 5.65-5.49 5.95.43.37.81 1.1.81 2.22 0 1.6-.01 2.88-.01 3.27 0 .32.22.69.82.58 4.77-1.59 8.21-6.09 8.21-11.39 0-6.63-5.37-12-12-12z"/>
         </svg>
       </a>
-      <a href="https://github.com/TheeFlakes" target="_blank" class="hover:text-purple-500">
+      <a href="https://github.com/TheeFlakes" target="_blank" class="hover:text-purple-500" aria-label="GitHub Profile 2">
         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 0c-6.63 0-12 5.37-12 12 0 5.3 3.44 9.8 8.21 11.39.6.11.82-.26.82-.58 0-.29-.01-1.05-.02-2.06-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.34-1.76-1.34-1.76-1.09-.75.08-.74.08-.74 1.2.08 1.83 1.23 1.83 1.23 1.07 1.83 2.81 1.3 3.5.99.11-.78.42-1.3.76-1.6-2.67-.3-5.47-1.34-5.47-5.95 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.17 0 0 1.01-.32 3.3 1.23.96-.27 1.98-.4 3-.4 1.02 0 2.04.14 3 .4 2.29-1.55 3.3-1.23 3.3-1.23.66 1.65.24 2.87.12 3.17.77.84 1.24 1.91 1.24 3.22 0 4.62-2.81 5.65-5.49 5.95.43.37.81 1.1.81 2.22 0 1.6-.01 2.88-.01 3.27 0 .32.22.69.82.58 4.77-1.59 8.21-6.09 8.21-11.39 0-6.63-5.37-12-12-12z"/>
         </svg>
       </a>
-      <a href="mailto:mmdennis25@gmail.com" class="hover:text-purple-500">
+      <a href="mailto:mmdennis25@gmail.com" class="hover:text-purple-500" aria-label="Email Denis">
         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 12.713l11.985-7.713c-.011-.011-11.985-7.713-11.985-7.713s-11.974 7.702-11.985 7.713l11.985 7.713zm0 2.287l-12-7.75v11.75c0 1.104.896 2 2 2h20c1.104 0 2-.896 2-2v-11.75l-12 7.75z"/>
         </svg>
